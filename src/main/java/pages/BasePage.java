2dc634ea1153309de3ac7b1a885be32442493f05
Ankelt <matt.jeevas@mail.ru>
2021-12-03 00:11:01 +0300

    add methods for base page

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,11 +1,15 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 import static constants.Constants.TimeoutVariable.EXPLICIT_WAIT;
+import static pages.locators.BasePageLocators.*;
+
 
 public class BasePage {
     protected WebDriver driver;
@@ -18,7 +22,56 @@ public class BasePage {
     }
 
     public WebElement waitElementIsVisible(WebElement element) {
-        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
+        wait.until(ExpectedConditions.visibilityOf(element));
         return element;
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isNotElementPresent(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        } catch (TimeoutException e) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDisappeared(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void goToLoginPage() {
+        WebElement login = driver.findElement(LOGIN_LINK);
+        login.click();
+    }
+
+    public void shouldBeLoginLink() {
+        Assert.assertTrue(isElementPresent(LOGIN_LINK), "Login link is not presented");
+    }
+
+    public void goToBasketPage() {
+        WebElement basket = driver.findElement(BASKET_LINK);
+        basket.click();
+    }
+
+    public void shouldBeAuthorised() {
+        Assert.assertTrue(isElementPresent(USER_ICON),
+                "User icon is not presented, probably unauthorised user");
     }
 }
---
+++ src/main/java/pages/locators/BasePageLocators.java
@@ -0,0 +1,9 @@
+package pages.locators;
+
+import org.openqa.selenium.By;
+
+public class BasePageLocators {
+    public static final By LOGIN_LINK = By.id("login_link");
+    public static final By BASKET_LINK = By.cssSelector(".basket-mini .btn-group .btn");
+    public static final By USER_ICON = By.cssSelector(".icon-user");
+}
