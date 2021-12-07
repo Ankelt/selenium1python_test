e75128e08ad368a0f4c6e35a2c63871ce800b6bd
Ankelt <matt.jeevas@mail.ru>
2021-12-07 15:56:04 +0300

    add methods for product page

---
+++ src/main/java/pages/ProductPage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import static pages.locators.ProductPageLocators.*;
+
+public class ProductPage extends BasePage{
+    public ProductPage(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+    public void addProductToBasket() {
+        WebElement basket_button = driver.findElement(BASKET_BUTTON);
+        basket_button.click();
+    }
+
+    public void shouldBeCorrectAdding() {
+        shouldBeMessageAboutAddingToBasket();
+        shouldBeEqualPriceForBasketAndProduct();
+    }
+
+    public void shouldBeEqualPriceForBasketAndProduct() {
+        WebElement price = driver.findElement(PRODUCT_PRICE);
+        WebElement basket = driver.findElement(PRICE_IN_BASKET);
+        String priceText = price.getText();
+        String basketText = basket.getText();
+        Assert.assertEquals(priceText, basketText,
+                String.format("Product price %s is not same with in basket (%s)", priceText, basketText));
+    }
+
+    public void shouldBeMessageAboutAddingToBasket() {
+        WebElement product = driver.findElement(PRODUCT_NAME);
+        WebElement productInBasket = driver.findElement(PRODUCT_IN_BASKET_NAME);
+        String productName = product.getText();
+        String productInBasketName = productInBasket.getText();
+        Assert.assertEquals(productName, productInBasketName,
+                String.format("Product name %s is not same with %s name in basket", productName, productInBasketName));
+    }
+
+    public void successMessageIsDisappeared() {
+        Assert.assertTrue(isDisappeared(SUCCESS_MESSAGE));
+    }
+
+    public void shouldBeSuccessMessage() {
+        Assert.assertTrue(isElementPresent(SUCCESS_MESSAGE));
+    }
+}
---
+++ src/main/java/pages/locators/ProductPageLocators.java
@@ -0,0 +1,12 @@
+package pages.locators;
+
+import org.openqa.selenium.By;
+
+public class ProductPageLocators {
+    public static final By BASKET_BUTTON = By.cssSelector("button.btn-add-to-basket");
+    public static final By PRODUCT_PRICE = By.cssSelector(".col-sm-6 > .price_color");
+    public static final By PRICE_IN_BASKET = By.cssSelector(".alert-info > .alertinner > p > strong");
+    public static final By PRODUCT_NAME = By.cssSelector(".col-sm-6 > h1");
+    public static final By PRODUCT_IN_BASKET_NAME = By.xpath("/div[@id='messages']/div[1]/div/strong");
+    public static final By SUCCESS_MESSAGE = By.cssSelector(".alert-success> .alertinner");
+}
